c495007c9941d879aea0fe360941e0af07920b13
brocjac <63880557+brocjac@users.noreply.github.com>
2026-08-05 15:00:02 -0500

    Clean up and database inserts

---
+++ Ice Depth Project/SQL Server Scripts/Ice_Depth_Insert.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import urllib
+from sqlalchemy import create_engine
+
+SERVER_NAME = 'YOUR_SERVER_NAME'
+DATABASE_NAME = 'YOUR_DATABASE_NAME'
+TABLE_NAME = 'your_target_table'
+CSV_FILE_PATH = 'data.csv'
